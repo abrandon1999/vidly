527ab8628a60d11d184f1ba675312fd0ffc55432
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-04 18:25:52 -0500

    express-server Listening port setup

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const app = express();
-console.log("Hello Node World")
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on Port ${port}...`));
