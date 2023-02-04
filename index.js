96d695554a30283f1399180059a6c7a9b3d63d15
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-04 18:35:27 -0500

    express-server response test Hello World

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req,res) => {
+    res.send('Hello Express World');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on Port ${port}...`));
