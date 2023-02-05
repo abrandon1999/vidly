15ede5ebb1253d6285bea76fc08c94b9e9c03c4a
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-04 22:33:07 -0500

    CRUD API-Genres POST-Request

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
-
+//-----------------------------------------------------------------------
+//MiddleWare
+app.use(express.json());
+//------------------------------------------------------------------------
+//Fake Database
 const genres = [
     {id: 1, name: "Action"},
     {id: 2, name: "Horror"},
     {id: 3, name: "Romance"}
 ];
 //-------------------------------------------------------------------------
+//GET Request for API Genres
 app.get('/api/genres', (req,res) => {
     res.send(genres);
 });
@@ -17,9 +22,18 @@ app.get('/api/genres/:id', (req,res) => {
     res.send(genre)
 });
 //-------------------------------------------------------------------------
+//POST Request for API Genres
+app.post('/api/genres',(req,res) => {
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+    genres.push(genre);
+    res.send(genre);
+});
 app.get('/', (req,res) => {
     res.send('Hello Express World!!!');
 });
-
+//--------------------------------------------------------------------------
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on Port ${port}...`));
