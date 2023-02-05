ab0b8874aa257dfcae453e34591ceade43e0de6d
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-04 22:16:02 -0500

    CRUD API-Genres GET-Request Route-Parameter

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const genres = [
     {id: 2, name: "Horror"},
     {id: 3, name: "Romance"}
 ];
-
+//-------------------------------------------------------------------------
 app.get('/api/genres', (req,res) => {
     res.send(genres);
 });
 
+app.get('/api/genres/:id', (req,res) => {
+    const genre = genres.find( g => g.id === parseInt(req.params.id));
+    if(!genre) res.status(404).send("Genre with Given ID was not Found");
+    res.send(genre)
+});
+//-------------------------------------------------------------------------
 app.get('/', (req,res) => {
     res.send('Hello Express World!!!');
 });
