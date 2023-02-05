51189155b56fcd0910928c807943bcd4bf07856c
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-04 19:40:15 -0500

    CRUD API-Genres GET-Request

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+const genres = [
+    {id: 1, name: "Action"},
+    {id: 2, name: "Horror"},
+    {id: 3, name: "Romance"}
+];
+
+app.get('/api/genres', (req,res) => {
+    res.send(genres);
+});
+
 app.get('/', (req,res) => {
     res.send('Hello Express World!!!');
 });
