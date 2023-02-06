f86342bb95a3fa59ed27202ce3256162fc7d6bb8
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-06 00:41:13 -0500

    CRUD API Movie POST-Request using mongoose

--- routes/movies.js
+++ routes/movies.js
@@ -2,12 +2,43 @@ const {Movie, validate} = require('../models/movies');
 const {Genre} = require('../models/genres');
 const express = require('express');
 const router = express.Router();
+//Middleware
+router.use(express.json());
 //----------------------------------------------------------
 //GET Request for API Movies
 router.get('/', async (req, res) => {
     const movies = await Movie.find().sort('name');
     res.send(movies);
   });
+
+  router.get('/:id', async (req, res) => {
+    const movie = await Movie.findById(req.params.id);
+  
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+  
+    res.send(movie);
+  });
 //----------------------------------------------------------
+//POST Request for API Movies
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send('Invalid genre.');
+  
+    let movie = new Movie({ 
+      title: req.body.title,
+      genre: {
+        _id: genre._id,
+        name: genre.name
+      },
+      numberInStock: req.body.numberInStock,
+      dailyRentalRate: req.body.dailyRentalRate
+    });
+    movie = await movie.save();
+    
+    res.send(movie);
+  });
 module.exports = router;
 //---------------------------------------------------------
