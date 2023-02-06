c03affeaa4357df8ecb4707154f989a381ef5ad6
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-06 03:00:06 -0500

    CRUD API Rental GET-Request Route-Parameter

--- routes/rentals.js
+++ routes/rentals.js
@@ -3,10 +3,18 @@ const {Movie} = require('../models/movie');
 const {Customer} = require('../models/customer');
 const express = require('express');
 const router = express.Router();
-
+//----------------------------------------------------------------------------
+//GET Request for API Rental
 router.get('/', async (req, res) => {
     const rentals = await Rental.find().sort('-dateOut');
     res.send(rentals);
   });
-
+router.get('/:id', async (req, res) => {
+    const rental = await Rental.findById(req.params.id);
+  
+    if (!rental) return res.status(404).send('The rental with the given ID was not found.');
+  
+    res.send(rental);
+  });
+//----------------------------------------------------------------------------
 module.exports = router; 
