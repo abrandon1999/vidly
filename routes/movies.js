a2ffed8288c6a940fabbe450f44b78cb880a081c
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-06 00:55:20 -0500

    CRUD API Movie PUT-Request using mongoose

--- routes/movies.js
+++ routes/movies.js
@@ -40,5 +40,30 @@ router.post('/', async (req, res) => {
     
     res.send(movie);
   });
+//---------------------------------------------------------
+//PUT Request for API Movies
+router.put('/:id', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send('Invalid genre.');
+  
+    const movie = await Movie.findByIdAndUpdate(req.params.id,
+      { 
+        title: req.body.title,
+        genre: {
+          _id: genre._id,
+          name: genre.name
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate
+      }, { new: true });
+  
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+    
+    res.send(movie);
+  });
+//--------------------------------------------------------
 module.exports = router;
 //---------------------------------------------------------
