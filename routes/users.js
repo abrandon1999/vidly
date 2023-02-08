2c3c7d1bac6240af615070ae30ad3c1441f52dec
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-08 16:43:18 -0500

    AAA using lodash for users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ const {User, validate} = require("../models/user");
 const express = require('express');
 const router = express.Router();
 router.use(express.json());
-
+const _ = require('lodash');
+//--------------------------------------------------------------------
 router.post('/', async (req, res) => {
     const {error} = validate(req.body);
    
@@ -11,12 +12,8 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({email: req.body.email});  
     if(user) return res.status(400).send("User already registered...")
 
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
+    user = new User(_.pick(req.body,["name","email","password"]))
     await user.save();
-    res.send(user);
+    res.send(_.pick(user, ['_id','name','email']));
 });
 module.exports = router;
