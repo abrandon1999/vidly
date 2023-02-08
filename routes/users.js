f94653689f8f990b90d5f91ca1b821fee073a404
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-08 17:13:02 -0500

    AAA hashing password for users.js

--- routes/users.js
+++ routes/users.js
@@ -4,15 +4,20 @@ const express = require('express');
 const router = express.Router();
 router.use(express.json());
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 //--------------------------------------------------------------------
 router.post('/', async (req, res) => {
     const {error} = validate(req.body);
    
     if(error) return res.status(400).send(error.details[0].message);
     let user = await User.findOne({email: req.body.email});  
-    if(user) return res.status(400).send("User already registered...")
+    if(user) return res.status(400).send("User already registered...");
 
-    user = new User(_.pick(req.body,["name","email","password"]))
+    user = new User(_.pick(req.body,["name","email","password"]));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password,salt);
+    //const hashed = await bcrypt.hash(user.password,salt);
+    //user.password = hashed;
     await user.save();
     res.send(_.pick(user, ['_id','name','email']));
 });
