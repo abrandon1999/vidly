a24b3d52d0449bf1fa7b04ebe8eee0eae335fa86
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-08 19:55:35 -0500

    AAA Setting Response Header

--- routes/users.js
+++ routes/users.js
@@ -5,20 +5,20 @@ const router = express.Router();
 router.use(express.json());
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
+const config = require('config');//<<--------------
+const jwt = require('jsonwebtoken');//<<------------
 //--------------------------------------------------------------------
 router.post('/', async (req, res) => {
     const {error} = validate(req.body);
-   
     if(error) return res.status(400).send(error.details[0].message);
+
     let user = await User.findOne({email: req.body.email});  
     if(user) return res.status(400).send("User already registered...");
-
     user = new User(_.pick(req.body,["name","email","password"]));
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password,salt);
-    //const hashed = await bcrypt.hash(user.password,salt);
-    //user.password = hashed;
     await user.save();
-    res.send(_.pick(user, ['_id','name','email']));
+    const token = jwt.sign({_id: user._id}, config.get('jwtPrivateKey'));
+    res.header('x-auth-token', token).send(_.pick(user, ['_id','name','email']));
 });
 module.exports = router;
