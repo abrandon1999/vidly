8d289811177b70dd35e0d89872a9c7f1166245ce
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-10 23:16:11 -0500

    Extraction: Extracting Config logic

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const app = express();
-const config = require('config');
+
 require('./startup/logging'); //logging should come first
 require('./startup/routes')(app);
 require('./startup/db')();
+require('./startup/config')();
 //----------------------------------------------------------------------
-if(!config.get('jwtPrivateKey')){
-    console.error('FATAL ERROR: jwtPrivateKey is not defined...');
-    process.exit(1)
-}
-//-----------------------------------------------------------------------
 //GET Request for API HOME
 app.get('/', (req,res) => {
     res.send('Hello Express World!!!');
