ebe7a8059bbce396a71c196cac10ac1c71fb4aaf
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-13 05:55:40 -0500

    Integration Test: Preparing the App

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-require('./startup/logging'); //logging should come first
+require('./startup/logging')(); //logging should come first
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
