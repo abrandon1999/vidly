dfe60581b02f5b7e37258147c183c20e015bf059
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-13 06:08:59 -0500

    Integration Test: Setting Up Test Db

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,10 @@
 //const winston = require('winston');
+const config = require('config');
 const mongoose = require('mongoose');
 module.exports = function () {
+    const db = config.get('db');
     mongoose.set('strictQuery',false);
-    mongoose.connect('mongodb://localhost/vidly')
-        .then(() => console.log("Successfully, Connected to MongoDB..."))
+    mongoose.connect(db)
+        .then(() => console.log(`Successfully, Connected to ${db}`))
        // .catch(err => console.log("Sorry, Could not connect to MongoDB...",err))
 }
