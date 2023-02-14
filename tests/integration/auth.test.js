9a502420c798951504866d087c642ed0e492b735
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-14 18:16:34 -0500

    Integration Test: Testing Auth Middleware

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+const {User} = require('../../models/user');
+const {Genre} = require('../../models/genre');
+let server;
+describe('auth middleware', () => {
+    beforeEach(() => server = require('../../index'));
+    afterEach(async() => {
+    await Genre.remove({});    
+    server.close();
+});
+    let token;
+    const exec = () => {
+       return request(server).post('/api/genres')
+                             .set('x-auth-token',token)
+                             .send({name: 'genre1'})
+    }
+    beforeEach(() => {
+        token = new User().generateAuthToken();
+    });
+    it('Should return 401 if no token is provided', async() => {
+        token = ""
+        const res = await exec();
+        expect(res.status).toBe(401);
+    });
+});
