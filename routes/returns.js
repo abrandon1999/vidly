1e5b2ac53ceb766de70bbfbdf58f43a0d02a98f0
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-16 00:09:34 -0500

    Test-Driven: Testing Valid Request

--- routes/returns.js
+++ routes/returns.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const router = express.Router();
 const {Rental} = require('../models/rental');
 
 router.use(express.json());
-router.post('/', async(req,res) => {
+router.post('/', auth, async(req,res) => {
    if(!req.header('x-auth-token')) return res.status(401).send('Unauthorized...');
    if(!req.body.customerId) return res.status(400).send("customerId Not Provided...");
    if(!req.body.movieId) return res.status(400).send("movieId Not Provided...");
@@ -14,7 +15,8 @@ router.post('/', async(req,res) => {
   });
   if(!rental) return res.status(404).send('Rental not found...');
   if(rental.dateReturned) return res.status(400).send('Return already processed');
-  return res.status(401)
+  return res.status(200).send();
+ // return res.status(401)
 });
 
 module.exports = router;
