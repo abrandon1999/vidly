0826a75828655597243b06af7fe975c20d317342
Brandon Rouse <abrandon1999@yahoo.com>
2023-02-16 22:27:11 -0500

    Deployment: Preparing App for Production

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 require('./startup/logging')(); //logging should come first
 require('./startup/routes')(app);
+require('./startup/prod')(app);
 require('./startup/db')();
 require('./startup/config')();
 //----------------------------------------------------------------------
@@ -14,5 +15,5 @@ app.get('/', (req,res) => {
 process.env.PORT = 5000;
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => console.log(`Listening on Port ${port}...`));
-server.close(); //For Test purpose only
+//server.close(); //For Test purpose only
 module.exports = server;
